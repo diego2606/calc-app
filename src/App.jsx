70486db8e44f69bcc26c0bb6f4fb8e2439d3d3ed
diego2606 <diego.velasquez.ore@gmail.com>
2024-08-05 12:40:58 -0500

    feat: :sparkles: Add Numbers component

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,22 @@ import React from "react";
 import MathOperations from "./components/MathOperations";
 import Result from "./components/Result";
 import Functions from "./components/Functions";
-import Button from "./components/Button/Button";
+import Numbers from "./components/Numbers";
+
 
 import './App.css'
+
 
 
 // generation de la function del component
 const App = () => {
 
-    const clickHandlerFunction = (text) => {
-        console.log(text)
-    }
 
     // Lo que ejecuta la funcion
     console.log('Render App')
     return (<main className="react-calculator">
-        <Result/>
-        <div className="numbers">
-            <Button text="1" clickHandler={clickHandlerFunction}/>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            
-        </div>
+        <Result value={undefined}/>
+        <Numbers onClickNumber={(number) => console.log('Click en number', number)}/>
         <Functions
             onContentClear={() => console.log('Content clear')}
             onDelete={() => console.log('Delete')}
