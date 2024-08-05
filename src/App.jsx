7d7e363e6e9216534e9ec9e7730d214fb6d242d9
diego2606 <diego.velasquez.ore@gmail.com>
2024-08-05 10:27:55 -0500

    Avance de la calculadora, creando los componentes App, Button, MathOperations y Result

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+// importation
+import React from "react";
+import MathOperations from "./components/MathOperations";
+import Result from "./components/Result";
+import Button from "./components/Button";
+import './App.css'
+
+
+// generation de la function del component
+const App = () => {
+
+    const clickHandlerFunction = (text) => {
+        console.log(text)
+    }
+
+    // Lo que ejecuta la funcion
+    console.log('Render App')
+    return (<main className="react-calculator">
+        <Result/>
+        <div className="numbers">
+            <Button text="1" clickHandler={clickHandlerFunction}/>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+            
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                remove
+            </button>
+        </div>
+        <MathOperations 
+            onClickOperation={operation => console.log('Operation:', operation)}
+            onClickEqual={equal => console.log('Equal:', equal)}
+        />
+    </main>)
+}
+
+// exportation
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+// import React
+import React from "react";
+import PropTypes from 'prop-types'
+// Component functional
+
+const Button = ({ type, text, clickHandler }) => {
+    return (
+        <button className={type} onClick={() => {clickHandler(text)}}>
+            <span>{text}</span>
+        </button>
+    )
+}
+
+Button.propTypes = {
+    type: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    clickHandler: PropTypes.func.isRequired
+}
+
+export default Button
