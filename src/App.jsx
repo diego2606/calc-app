aba75b62ce3e0b959432547fc5ab96fee9afea25
diego2606 <diego.velasquez.ore@gmail.com>
2024-08-08 15:13:21 -0500

    feat: :art: add useState with array destructuring in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // importation
-import React from "react";
+import React, { useState } from "react";
 import MathOperations from "./components/MathOperations";
 import Result from "./components/Result";
 import Functions from "./components/Functions";
@@ -13,12 +13,21 @@ import './App.css'
 // generation de la function del component
 const App = () => {
 
+    // 1er position: valor (que inicialmente es el valor por defect)
+    // 2da position: function que me va a permitir modificar el valor por defecto
+    // [xxx], [setxxxx]
+    const [stack, setStack] = useState('')
+
 
     // Lo que ejecuta la funcion
     console.log('Render App')
     return (<main className="react-calculator">
-        <Result value={undefined}/>
-        <Numbers onClickNumber={(number) => console.log('Click en number', number)}/>
+        <Result value={stack}/>
+        <Numbers onClickNumber={number => {
+                console.log('Click en number', number)
+                setStack(number)
+            }}
+        />
         <Functions
             onContentClear={() => console.log('Content clear')}
             onDelete={() => console.log('Delete')}
